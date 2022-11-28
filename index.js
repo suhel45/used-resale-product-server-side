408060d1d0ce1ef23656e63717e43050697a00f4
suhel45 <suhel.ict45@gmail.com>
2022-11-29 00:49:02 +0600

    productCollection added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
 require('dotenv').config()
 
 const port = process.env.PORT || 5000;
@@ -23,6 +24,8 @@ async function run(){
     try{
         const durantaCollections = client.db('Bi-cycle-stores').collection('duranta');
         const bookingsCollections = client.db('Bi-cycle-stores').collection('booking');
+        const usersCollections = client.db('Bi-cycle-stores').collection('users');
+        const productsCollections = client.db('Bi-cycle-stores').collection('products');
         app.get('/duranta',async(req,res)=>{
             const query = { }
             const result = await durantaCollections.find(query).toArray();
@@ -32,6 +35,52 @@ async function run(){
             const booking = req.body;
             const result = await bookingsCollections.insertOne(booking);
             res.send(result);
+        })
+
+        app.post('/users',async(req,res)=>{
+            const user = req.body;
+            const result = await usersCollections.insertOne(user);
+            res.send(result);
+            console.log(result)
+        })
+
+        app.get('/orders',async(req,res)=>{
+            const email = req.query.email;
+            const query={
+                email:email
+            }
+            const order = await bookingsCollections.find(query).toArray();
+            res.send(order);
+        })
+
+        app.get('/sellers',async(req,res)=>{
+            const query = {
+                role:'Seller'
+            }
+            const result = await usersCollections.find(query).toArray();
+            res.send(result);
+        })
+
+        app.get('/buyers',async(req,res)=>{
+            const query = {
+                role:'User'
+            }
+            const result = await usersCollections.find(query).toArray();
+            res.send(result);
+        })
+
+        app.post('/products',async(req,res)=>{
+            const products = req.body;
+            const result = await productsCollections.insertOne(products);
+            res.send(result);
+            console.log(result);
+        })
+
+
+        app.post('/jwt',(req,res)=>{
+            const user = req.body;
+            const token = jwt.sign(user,process.env.ACCESS_TOKEN,{expiresIn:'360d'})
+            res.send({token})
         })
     }
     finally{
